035f935b36fba2def2b3014d11bbcfc9356ba7b0
Masum Talukder <mdmasum.shuvo.cse@gmail.com>
2021-11-29 17:44:58 +0600

    Initial commit

---
+++ app/src/main/java/com/appinionbd/jepackcompose/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.appinionbd.jepackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.appinionbd.jepackcompose.ui.theme.JepackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JepackComposeTheme {
+                myApp()
+            }
+        }
+    }
+}
+
+@Composable
+private fun myApp(names: List<String> = listOf("world", "compose")) {
+   Column(modifier = Modifier.padding(4.dp)){
+       for (name in names) {
+           Greeting(name)
+       }
+   }
+
+}
+
+@Composable
+fun Greeting(name: String) {
+    Surface(color = MaterialTheme.colors.primary,modifier = Modifier.padding(vertical = 4.dp,horizontal = 8.dp)) {
+        Row(modifier= Modifier.padding(24.dp)) {
+            Column(modifier =Modifier.weight(1f)) {
+                Text(text = "Hello $name")
+            }
+            OutlinedButton(onClick = { /*TODO*/ }) {
+                Text("See more")
+            }
+        }
+
+
+    }
+
+}
+
+@Preview(showSystemUi = true,showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JepackComposeTheme {
+        myApp()
+    }
+}
