7e203f16986561d7986d57be0635f8eeb4f2b24e
Masum <masum.talukder@appinionbd.com>
2021-12-12 13:18:14 +0600

    complete codelab

--- app/src/main/java/com/appinionbd/jepackcompose/ui/theme/Theme.kt
+++ app/src/main/java/com/appinionbd/jepackcompose/ui/theme/Theme.kt
@@ -2,20 +2,27 @@ package com.appinionbd.jepackcompose.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.material.MaterialTheme.shapes
+import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    surface = Blue,
+    onSurface = Navy,
+    primary = Navy,
+    onPrimary = Chartreuse
+
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    surface = Blue,
+    onSurface = Color.White,
+    primary = LightBlue,
+    onPrimary = Navy
 
     /* Other default colors to override
     background = Color.White,
@@ -28,7 +35,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun JepackComposeTheme(
+fun JetpackComposeTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable() () -> Unit
 ) {
@@ -44,4 +51,24 @@ fun JepackComposeTheme(
         shapes = Shapes,
         content = content
     )
+}
+
+@Composable
+fun BasicCodelabTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val color = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+    MaterialTheme(
+        colors = color,
+        typography = typography,
+        shapes = shapes,
+        content = content
+    )
+
+
 }
